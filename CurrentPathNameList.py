f88da4356ca16bbe4827c484de44d84d0eb08c07
cairongfu <q0975040879@gmail.com>
2021-01-12 14:18:24 +0800

    'v2'

--- CurrentPathNameList.py
+++ CurrentPathNameList.py
@@ -4,15 +4,20 @@ path = "./"
 files = os.listdir(path)
 
 def writetxt(f):
-    WriteFile = open("List.txt",'a')
+    WriteFile = open("List.txt",'a',encoding="utf-8")
     WriteFile.write(f)
     WriteFile.write('\n')
     WriteFile.close()
 
 for f in files:
-  fullpath = os.path.join(path, f)
-  if os.path.isfile(fullpath):
-    writetxt(f)
-  elif os.path.isdir(fullpath):
-    pass
+    fullpath = os.path.join(path, f)
+    if os.path.isfile(fullpath):
+        if f == "CurrentPathNameList.py":
+            pass
+        elif f == "List.txt":
+            pass
+        else:
+            writetxt(f)
+    elif os.path.isdir(fullpath):
+        pass
 
